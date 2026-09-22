752aab6293e8b02227bca29609bea80f35654561
Midul Mahmud <midul.mahmud20@gmail.com>
2026-09-22 14:31:23 +0600

    fixed

--- src/lib/BooksData.tsx
+++ src/lib/BooksData.tsx
@@ -1,6 +1,15 @@
-const getBooks = async () => {
-  const res = await fetch("http://localhost:3000/booksData.json");
-  return res.json();
+import { IBook } from "@/types/bookTypes";
+
+const getBooks = async (): Promise<IBook[]> => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`,
+    );
+    return res.json();
+  } catch (error) {
+    console.error("Error fetching books data:", error);
+    return [];
+  }
 };
 
 export default getBooks;
